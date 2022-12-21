c3eab566f9108f47227ea1608ed4187c6fedc5d3
denisstocki <268439@student.pwr.edu.pl>
2022-12-21 12:28:17 +0100

    Dodanie struktury projektu.

---
+++ klient/src/main/java/tp/warcaby/klient/App.java
@@ -0,0 +1,13 @@
+package tp.warcaby.klient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ serwer/src/main/java/tp/warcaby/serwer/App.java
@@ -0,0 +1,13 @@
+package tp.warcaby.serwer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

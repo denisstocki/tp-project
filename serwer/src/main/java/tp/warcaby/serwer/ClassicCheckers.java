208a20138c6113fa15e41fb9986f4ed1a0370218
denisstocki <268439@student.pwr.edu.pl>
2022-12-26 11:06:03 +0100

    Dodanie metody sprawdzajacej ruch dla klasycznej gry

--- serwer/src/main/java/tp/warcaby/serwer/ClassicCheckers.java
+++ serwer/src/main/java/tp/warcaby/serwer/ClassicCheckers.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class ClassicCheckers implements GameLogic{
 
-    private List<List<Character>> board;
+    private List<List<String>> board;
     private int white_count;
     private int black_count;
 
@@ -23,25 +23,25 @@ public class ClassicCheckers implements GameLogic{
 
     @Override
     public void initializeBoard() {
-        ArrayList<Character> rowArray;
+        ArrayList<String> rowArray;
         board = new ArrayList<>(8);
         for (int row = 0; row < 8; row++) {
-            rowArray = new ArrayList<Character>(8);
+            rowArray = new ArrayList<String>(8);
             board.add(rowArray);
             if(row<3){
                 for (int col = 0; col < 8; col++) {
-                    if((row+col) % 2 == 1) rowArray.add('B');
-                    else rowArray.add('E');
+                    if((row+col) % 2 == 1) rowArray.add("B");
+                    else rowArray.add("E");
                 }
             }else if(row>4){
                 for (int col = 0; col < 8; col++) {
-                    if((row+col) % 2 == 1) rowArray.add('W');
-                    else rowArray.add('E');
+                    if((row+col) % 2 == 1) rowArray.add("W");
+                    else rowArray.add("E");
                 }
             }
             else{
                 for (int col = 0; col < 8; col++) {
-                    rowArray.add('E');
+                    rowArray.add("E");
                 }
             }
         }
@@ -54,20 +54,62 @@ public class ClassicCheckers implements GameLogic{
         int x2 = Integer.parseInt(String.valueOf(move.charAt(2)));
         int y2 = Integer.parseInt(String.valueOf(move.charAt(3)));
         int x0, y0;
-        char pawn = board.get(x1).get(y1), jumped;
-        char direction = board.get(x2).get(y2);
+        boolean score = false;
+        String pawn = board.get(x1).get(y1), jumped;
+        String direction = board.get(x2).get(y2);
         System.out.println("Checking pawn " + pawn + " moving from " + x1 +"," + y1 + " to " + x2 + "," + y2);
-        if(pawn == 'B' || pawn == 'W'){
-            if(Math.abs(x2-x1)==1 && Math.abs(y2-y1)==1 && direction == 'E') return true;
-            if(Math.abs(x2-x1)==2 && Math.abs(y2-y1)==2 && direction == 'E'){
+        if(pawn == "B" || pawn == "W"){
+            if(Math.abs(x2-x1)==1 && Math.abs(y2-y1)==1 && direction == "E") score = true;
+            else if(Math.abs(x2-x1)==2 && Math.abs(y2-y1)==2 && direction == "E"){
                 if(x2>x1) x0 = x1+1;
                 else x0 = x1-1;
                 if(y2>y1) y0 = y1+1;
                 else y0 = y1-1;
                 jumped = board.get(x0).get(y0);
-                if(pawn == 'B' && jumped == 'W') return true;
-                if(pawn == 'W' && jumped == 'B') return true;
+                if(pawn == "B" && jumped == "W") score = true;
+                else if(pawn == "W" && jumped == "B") score =  true;
+                else score = false;
             }
+            else score = false;
+        }
+        else if(pawn == "BQ" || pawn == "WQ"){
+            if(Math.abs(x2-x1)==Math.abs(y2-y1) && direction == "E" && (x2 != x1) && (y2 != y1)){
+                if(pawn == "WQ" && !isTraitor("WQ", x1, x2, y1, y2) && isFairFight("WQ", x1, x2, y1, y2)) score = true;
+                else if(pawn == "BQ" && !isTraitor("BQ", x1, x2, y1, y2) && isFairFight("BQ", x1, x2, y1, y2)) score = true;
+                else score = false;
+            }
+        }
+        return score;
+    }
+
+    private boolean isFairFight(String queen, int x1, int x2, int y1, int y2) {
+        int dx, dy;
+        if(x2>x1) dx=1;
+        else dx=-1;
+        if(y2>y1) dy=1;
+        else dy=-1;
+        x1 += dx;
+        y1 += dy;
+        int counter = 0;
+        String target;
+        if(queen.charAt(0) == 'W') target = "B";
+        else target = "W";
+        while(x1 != x2 && y1 != y2){
+            if(board.get(x1).get(y1) == target) counter++;
+        }
+        return counter<=1;
+    }
+
+    private boolean isTraitor(String queen, int x1, int x2, int y1, int y2) {
+        int dx, dy;
+        if(x2>x1) dx=1;
+        else dx=-1;
+        if(y2>y1) dy=1;
+        else dy=-1;
+        x1 += dx;
+        y1 += dy;
+        while(x1 != x2 && y1 != y2){
+            if(board.get(x1).get(y1) == String.valueOf(queen.charAt(0))) return true;
         }
         return false;
     }
@@ -79,7 +121,7 @@ public class ClassicCheckers implements GameLogic{
 
     @Override
     public boolean gameFinished() {
-        return white_count == 0 || black_count == 0;
+        return white_count == 0 || black_count == 0 || getPossibleMovesFor();
     }
 
     @Override
@@ -96,9 +138,9 @@ public class ClassicCheckers implements GameLogic{
 
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("ClassicCheckers{\n");
+        final StringBuffer sb = new StringBuffer();
         for (int i = 0; i < 8; i++) {
-            List<Character> rowArray = board.get(i);
+            List<String> rowArray = board.get(i);
             for (int j = 0; j < 8; j++) {
                 sb.append(rowArray.get(j) + " ");
             }

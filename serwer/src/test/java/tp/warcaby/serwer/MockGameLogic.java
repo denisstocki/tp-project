15a9a95b9963ce9d3d7feb6921d5d1630014782b
MrKubul <jakub.dryka1@gmail.com>
2023-01-13 00:48:57 +0100

    Move decoder tests, Mocks of Logics tests

---
+++ serwer/src/test/java/tp/warcaby/serwer/MockGameLogic.java
@@ -0,0 +1,8 @@
+package tp.warcaby.serwer;
+
+public class MockGameLogic extends ClassicLogic {
+
+    public MockGameLogic(int size, int whiteCount, int blackCount) {
+        super(size, whiteCount, blackCount);
+    }
+}
